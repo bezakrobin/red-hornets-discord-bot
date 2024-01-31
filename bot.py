b24c70f6e3163ef7bbaca0ee32371ef4af2bfb51
Robin Bezak <bezakrobin@gmail.com>
2024-01-31 02:43:48 +0100

    added embed welcome message
    Took 11 minutes

--- bot.py
+++ bot.py
@@ -11,13 +11,30 @@ def home():
     return "I'm alive!"
 
 
+# ENV VARIABLES
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 GUILD_ID = os.environ.get('GUILD_ID')
 WELCOME_CHANNEL_ID = os.environ.get('WELCOME_CHANNEL_ID')
 
+# BOT SETUP
 intents = discord.Intents.default()
+intents.members = True
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
+
+
+# EMBED WELCOME MESSAGE
+@client.event
+async def on_member_join(member):
+    welcome_channel = client.get_channel(int(WELCOME_CHANNEL_ID))
+    if welcome_channel:
+        embed = discord.Embed(
+            title=f"Welcome {member.name}!",
+            description=f"Thanks for joining {member.guild.name}! We're glad to have you here.",
+            color=discord.Color.green()
+        )
+        embed.set_thumbnail(url=member.avatar.url if member.avatar else member.default_avatar.url)
+        await welcome_channel.send(embed=embed)
 
 
 @tree.command(
