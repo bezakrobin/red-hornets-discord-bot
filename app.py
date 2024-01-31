3293e0582a0e3f89e04e38e69c060e518fab33cf
Robin Bezak <bezakrobin@gmail.com>
2024-01-31 04:01:26 +0100

    added member count into data.json created load and save data fucntions welcome message refactored into function
    Took 13 minutes

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from discord import app_commands
 import os
 from flask import Flask
 from threading import Thread
+import json
+
 
 # FLASK WEB SERVER
 app = Flask(__name__)
@@ -26,6 +28,8 @@ def keep_alive():
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 GUILD_ID = os.environ.get('GUILD_ID')
 WELCOME_CHANNEL_ID = os.environ.get('WELCOME_CHANNEL_ID')
+SERVER_STATS_CATEGORY_ID = os.environ.get('SERVER_STATS_CATEGORY_ID')
+
 
 # BOT SETUP
 intents = discord.Intents.default()
@@ -37,15 +41,7 @@ tree = app_commands.CommandTree(client)
 # ON MEMBER JOIN EVENT
 @client.event
 async def on_member_join(member):
-    welcome_channel = client.get_channel(int(WELCOME_CHANNEL_ID))
-    if welcome_channel:
-        embed = discord.Embed(
-            title=f"Welcome @{member.name}!",
-            description=f"Thanks for joining {member.guild.name}! We're glad to have you here.",
-            color=discord.Color.red()
-        )
-        embed.set_thumbnail(url=member.avatar.url if member.avatar else member.default_avatar.url)
-        await welcome_channel.send(embed=embed)
+    send_welcome_message(member)
 
 
 # SLASH COMMANDS
@@ -65,5 +61,35 @@ async def on_ready():
     print("Ready!")
 
 
+# FLASK & BOT START
 keep_alive()
 client.run(BOT_TOKEN)
+
+
+# FUNCTIONS
+# LOAD JSON DATA
+def load_data(file_name='data.json'):
+    try:
+        with open(file_name, 'r') as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return {}
+
+
+# SAVE JSON DATA
+def save_data(data, file_name='data.json'):
+    with open(file_name, 'w') as file:
+        json.dump(data, file, indent=4)
+
+
+# SEND WELCOME MESSAGE
+async def send_welcome_message(member):
+    welcome_channel = client.get_channel(int(WELCOME_CHANNEL_ID))
+    if welcome_channel:
+        embed = discord.Embed(
+            title=f"Welcome @{member.name}!",
+            description=f"Thanks for joining {member.guild.name}! We're glad to have you here.",
+            color=discord.Color.red()
+        )
+        embed.set_thumbnail(url=member.avatar.url if member.avatar else member.default_avatar.url)
+        await welcome_channel.send(embed=embed)
