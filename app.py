d2d4a26bfe9a820e3bf0553375b90255fe011774
Robin Bezak <bezakrobin@gmail.com>
2024-01-31 03:48:45 +0100

    welcome message changed color to red
    Took 2 minutes

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ async def on_member_join(member):
         embed = discord.Embed(
             title=f"Welcome @{member.name}!",
             description=f"Thanks for joining {member.guild.name}! We're glad to have you here.",
-            color=discord.Color.green()
+            color=discord.Color.red()
         )
         embed.set_thumbnail(url=member.avatar.url if member.avatar else member.default_avatar.url)
         await welcome_channel.send(embed=embed)
