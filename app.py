06faa59ac1f883b93a927d0dbc833d3f15b7298b
Robin Bezak <bezakrobin@gmail.com>
2024-01-31 03:46:34 +0100

    tag the newcomer
    Took 11 minutes

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ async def on_member_join(member):
     welcome_channel = client.get_channel(int(WELCOME_CHANNEL_ID))
     if welcome_channel:
         embed = discord.Embed(
-            title=f"Welcome {member.name}!",
+            title=f"Welcome @{member.name}!",
             description=f"Thanks for joining {member.guild.name}! We're glad to have you here.",
             color=discord.Color.green()
         )
