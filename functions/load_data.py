181a8ead77d7ba27763867752d754b855120929e
Robin Bezak <bezakrobin@gmail.com>
2024-01-31 08:04:49 +0100

    hot fix
    Took 3 minutes

--- functions/load_data.py
+++ functions/load_data.py
@@ -1,7 +1,7 @@
 import json
 
 
-def load_data(file_name='data.json'):
+def load_data(file_name='../data.json'):
     try:
         with open(file_name, 'r') as file:
             return json.load(file)
